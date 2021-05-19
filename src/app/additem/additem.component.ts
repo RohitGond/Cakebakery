8b01003bdbee6b94298ca44eab3c2417f8ec222c
unknown <gondrohit4@gmail.com>
2021-05-19 16:18:24 +0530

    img upload updated

--- src/app/additem/additem.component.ts
+++ src/app/additem/additem.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { log } from 'node:console';
 
 
 @Component({
@@ -7,12 +11,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./additem.component.css']
 })
 export class AdditemComponent implements OnInit {
-  cake: any = {};
-  constructor() { }
+  cake: any = {type : "" };
+  
+  constructor( private routes : ActivatedRoute, private route : Router, private http : HttpClient , private toast : ToastrService) { 
+    
+  }
 
   ngOnInit(): void {
   }
   addItem() {
-    console.log(this.cake);
+    const api = 'https://apifromashu.herokuapp.com/api/addcake';
+    this.http.post(api, this.cake).subscribe((res: any)=>{
+      console.log(res.message);
+      
+    },
+     (error)=> {console.log(error);})
+  }
+  upload(e: any){
+    console.log(e.target);
+    const inputimg =  e.target.files[0], previewimg = e.target.parentElement.querySelector("img"),
+    formData : any = new FormData();
+    formData.append("file", inputimg);
+    this.http.post('https://apifromashu.herokuapp.com/api/upload', formData).subscribe((res : any) => {
+      if(res.imageUrl){
+        this.toast.success("image uploaded");
+        this.cake.image = res.imageUrl;
+      }
+      else{
+        this.toast.warning("image upload failed");
+      }
+
+    }, (error) => {
+      console.log(error);
+      
+    })
+    console.log(inputimg);
+    
+
   }
 }
